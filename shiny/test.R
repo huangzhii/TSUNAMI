9157dc8d6e51bd82b962c76b51b2c8ad4b79015c
Zhi Huang <zhi@Zhis-MacBook-Pro.local>
2018-01-18 06:37:49 -0500

    finish step 1

--- shiny/test.R
+++ shiny/test.R
@@ -1,9 +1,31 @@
 library(genefilter)
-setwd("/media/zhi/Drive3/GeneCoexpression/matlab_old");
-data<-read.csv("./RNAdata.csv", header=T, stringsAsFactors=F)
-
+library(Biobase)
+# setwd("/media/zhi/Drive3/GeneCoexpression/matlab_old");
+setwd("/Users/zhi/Desktop/GeneCoexpression/shiny"); #mac
+source("utils.R")
+data<-read.csv("../matlab_old/RNAdata.csv", header=T, stringsAsFactors=F)
 
 # Step 1
-RNA <- data.frame(data[1:dim(data)[1], 2:dim(data)[2]])
-
+RNA <- as.matrix(data[1:dim(data)[1], 2:dim(data)[2]])
 geneID <- data.frame(data[1:dim(data)[1], 1])
+# Remove data with lowest 20% absolute exp value shared by all samples
+RNA_filtered1 = RNA[apply(RNA,1,max) > quantile(RNA, 0.2)[[1]], ]
+geneID_filtered1 = geneID[apply(RNA,1,max) > quantile(RNA, 0.2)[[1]], ]
+# Remove data with lowest 10% variance across samples
+index <- varFilter2(eset = RNA_filtered1, var.cutoff = 0.1)
+RNA_filtered2 = RNA_filtered1[index, ]
+geneID_filtered2 = geneID_filtered1[index]
+expData <- RNA_filtered2
+res <- highExpressionProbes(geneID_filtered2, geneID_filtered2, expData)
+ind1 <- res$first
+uniGene <- res$second
+tmpExp <-expData[ind1,]
+nSample <- ncol(tmpExp)
+res <- sort.int(rowMeans(tmpExp), index.return=TRUE)
+sortMean <- res$x
+sortInd <- res$ix
+topN <- min(20000, nrow(tmpExp))
+finalExp <- tmpExp[sortInd[1:topN], ]
+finalSym <- uniGene[sortInd[1:topN]]
+cMatrix <- massivePCC_withoutNaN(finalExp)
+cMatrix[1 : (nrow(cMatrix)+1) : length(cMatrix)] <- 0
---
+++ shiny/utils.R
@@ -0,0 +1,74 @@
+# 01/18/2018 Zhi Huang
+library(genefilter)
+library(nnet)
+library(parallel)
+varFilter2 <- function (eset, var.func = IQR, var.cutoff = 0.5, filterByQuantile = TRUE) {
+  if (deparse(substitute(var.func)) == "IQR") {
+    vars <- genefilter:::rowIQRs(eset)
+  }
+  else {
+    vars <- apply(exprs(eset), 1, var.func)
+  }
+  if (filterByQuantile) {
+    if (0 < var.cutoff && var.cutoff < 1) {
+      quant = quantile(vars, probs = var.cutoff)
+      selected = !is.na(vars) & vars > quant
+    }
+    else stop("Cutoff Quantile has to be between 0 and 1.")
+  }
+  else {
+    selected <- !is.na(vars) & vars > var.cutoff
+  }
+  return(selected)
+}
+
+highExpressionProbes <- function (genes, probes, eset){
+  meanData <- rowMeans(eset)
+  res <- sort.int(genes, index.return=TRUE)
+  sGenes <- res$x
+  sInd <- res$ix
+  sProbes <- probes[sInd]
+  sMean <- meanData[sInd]
+  unik <- !duplicated(sGenes)
+  uniInd <- seq_along(sGenes)[unik]  ## indices
+  uniGene <- sGenes[unik] ## the values
+  uniInd <- append(uniInd, 0, after = 0)
+  tmpInd <- vector(mode="numeric", length=length(uniGene))
+  for (i in 1:(length(uniInd)-1)){
+    a <- uniInd[i]+1
+    b <- uniInd[i+1]
+    # maxV <- max(sMean[a:b])
+    tmpInd[i] <- uniInd[i] + which.is.max(sMean[a:b])
+  }
+  ind <- sInd[tmpInd]
+  return(list(first=ind, second=uniGene))
+}
+
+
+massivePCC_withoutNaN <- function (eset){
+  nRow <- nrow(eset)
+  nCol <- ncol(eset)
+  eset[is.na(eset)] <- 0
+  if (nRow > 1) {
+    sumX <- rowSums(eset)
+    # meanX <- colMeans(eset)
+    # A * B	Element-wise multiplication
+    sumV <- sqrt( rowSums(eset * eset) - (sumX * sumX)/nCol )
+    PCC_mat <- matrix(, nrow = nRow, ncol = nRow)
+    
+    #starting parallel
+    # no_cores <- detectCores() - 1
+    # cl <- makeCluster(no_cores)
+    
+    for (i in 1:(nRow-1)){
+      if (i %% round(nRow/100) == 0) {
+        print(sprintf("Processing Massive PCC ... %.2f%%",(i/nRow*100)))
+      }
+      # %*% Matrix multiplication
+      # PCC_mat[i,(i+1):nRow] <- ( eset[(i+1):nRow,] %*% eset[i,] - sumX[i] * sumX[(i+1):nRow]/nCol ) / (sumV[i] * sumV[(i+1):nRow])
+      PCC_mat[i,(i+1):nRow] <- ( tcrossprod(eset[(i+1):nRow,],t(eset[i,])) - sumX[i] * sumX[(i+1):nRow]/nCol ) / (sumV[i] * sumV[(i+1):nRow])
+    }
+    PCC_mat <- PCC_mat + t(PCC_mat)
+    
+  }
+}
