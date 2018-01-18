946659de22a841bd55b0084243f10d6ee47f0b06
Jeremy Zhi Huang <huangzz@umich.edu>
2018-01-18 00:46:26 -0500

    add R files

---
+++ shiny/GeneCoExpressionAnalysis.R
@@ -0,0 +1,29 @@
+# 01/17/2018 Zhi Huang
+lmQCM_preprocess <- function(data) {
+  # Step 1
+  RNA <- as.numeric(data[2:dim(data)[1], 2:dim(data)[2]])
+  geneID <- as.numeric(data[2:dim(data)[1], 1])
+  
+  
+  
+  return(666)
+}
+
+
+
+lmQCM <- function(cMatrix, gamma, lambda, t, beta, minClusterSize) {
+  print(sprintf("gamma: %.2f",gamma))
+  print(sprintf("lambda: %.2f",lambda))
+  print(sprintf("t: %.2f",t))
+  print(sprintf("beta: %.2f",beta))
+  print(sprintf("minClusterSize: %d",minClusterSize))
+  
+  
+  
+  
+  
+  
+  
+  
+  return(666)
+}
---
+++ shiny/main.R
@@ -0,0 +1,145 @@
+library(shiny)
+
+options(shiny.maxRequestSize=100*1024^2) # to the top of server.R would increase the limit to 300MB
+
+
+# Define UI for dataset viewer app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("lmQCM for mining the locally dense structures"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      # Input: Select a file ----
+      fileInput("csvfile", "Choose CSV File",
+                multiple = TRUE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
+      # Include clarifying text ----
+      helpText("Note: Maximum csv file size allowed for uploading is 300MB."),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+      
+      # Horizontal line ----
+      tags$hr(),
+      # Input: Select a method ----
+      selectInput("method", "Choose a method:",
+                  choices = c("lmQCM", "WGCNA")),
+      
+      # Input: Specify the number of observations to view ----
+      numericInput("gamma", "gamma:", 0.55),
+      numericInput("lambda", "lambda", 1),
+      numericInput("t", "t:", 1),
+      numericInput("beta", "beta:", 0.4),
+      numericInput("minClusterSize", "Minimum Cluster Size:", 10),
+      
+      
+      # Input: actionButton() to defer the rendering of output ----
+      # until the user explicitly clicks the button (rather than
+      # doing it immediately when inputs change). This is useful if
+      # the computations required to render output are inordinately
+      # time-consuming.
+      actionButton("update", "Update View")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Header + summary of distribution ----
+      h4("Summary"),
+      verbatimTextOutput("summary"),
+      
+      # Output: Header + table of distribution ----
+      h4("Observations"),
+      tableOutput("view")
+    )
+    
+  )
+)
+
+# Define server logic to summarize and view selected dataset ----
+server <- function(input, output) {
+  
+  
+  method <- eventReactive(input$update, {
+    switch(input$method,
+           "lmQCM" = "lmQCM",
+           "WGCNA" = "WGCNA")
+  }, ignoreNULL = FALSE)
+  
+  csvfile <- eventReactive(input$update, {
+    req(input$csvfile)
+    data <- read.csv(input$csvfile$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote)
+  }, ignoreNULL = FALSE)
+  
+  gamma <- eventReactive(input$update, {
+    input$gamma
+  }, ignoreNULL = FALSE)
+  lambda <- eventReactive(input$update, {
+    input$lambda
+  }, ignoreNULL = FALSE)
+  t <- eventReactive(input$update, {
+    input$t
+  }, ignoreNULL = FALSE)
+  beta <- eventReactive(input$update, {
+    input$beta
+  }, ignoreNULL = FALSE)
+  minClusterSize <- eventReactive(input$update, {
+    input$minClusterSize
+  }, ignoreNULL = FALSE)
+  
+  # Generate a summary of the dataset ----
+  output$summary <- renderPrint({
+    if (method() == "lmQCM"){
+      source("GeneCoExpressionAnalysis.R")
+      # Check if is a new data, then determine to perform lmQCM_1 or not.
+      cMatrix = lmQCM_preprocess(data())
+      
+      k = lmQCM(cMatrix, gamma(), lambda(), t(), beta(), minClusterSize())
+      method()
+      # Run GeneCoExpressionAnaylsis
+    } else {
+      method()
+      # Run WGCNA
+    }
+  })
+  
+  # Show the first "n" observations ----
+  # The use of isolate() is necessary because we don't want the table
+  # to update whenever input$obs changes (only when the user clicks
+  # the action button)
+  
+  # obs = 10
+  # output$view <- renderTable({
+  #   head(datasetInput(), n = isolate(input$obs))
+  # })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
