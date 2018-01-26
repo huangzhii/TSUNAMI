843e5f39c0908b6920f24d19a3d2eefe40bc0603
Zhi Huang <huangzz@umich.edu>
2018-01-26 15:18:40 -0500

    add RGUI and shiny examples

---
+++ shiny-examples-master/055-observer-demo/server.R
@@ -0,0 +1,27 @@
+function(input, output, session) {
+
+  # Create a random name for the log file
+  logfilename <- paste0('logfile',
+                        floor(runif(1, 1e+05, 1e+06 - 1)),
+                        '.txt')
+
+
+  # This observer adds an entry to the log file every time
+  # input$n changes.
+  obs <- observe({    
+    cat(input$n, '\n', file = logfilename, append = TRUE)
+  })
+
+
+  session$onSessionEnded(function() {
+    # When the client ends the session, clean up the log file
+    # for this session.
+    unlink(logfilename)
+  })
+
+
+  output$text <- renderText({
+    paste0("The value of input$n is: ", input$n)
+  })
+
+}
